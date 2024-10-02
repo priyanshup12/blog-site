a9f99ea778d5b50ad16a24481465f06722169f2c
priyanshup12 <priyanshupateriya150@gmail.com>
2024-10-02 12:31:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views")); 
 let blog_ids = 1;
-app.use(express.static(path.join(__dirname, "/public"));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 const storage = multer.diskStorage({
